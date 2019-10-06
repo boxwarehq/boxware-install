2d4c092ee079a3775f40cda8f2fde40554f50e8e
Somesh Kar <someshkar04@gmail.com>
2019-10-06 23:22:28 +0530

    this still refuses to work

--- public/install.sh
+++ public/install.sh
@@ -5,8 +5,8 @@ echo "Installing Boxware now"
 
 mkdir $HOME/.boxware
 
-wget http://files.somesh.co/boxware/boxware -o $HOME/.boxware/boxware
-wget http://files.somesh.co/boxware/boxwared -o $HOME/.boxware/boxwared
+wget http://files.somesh.co/boxware/boxware -O $HOME/.boxware/boxware
+wget http://files.somesh.co/boxware/boxwared -O $HOME/.boxware/boxwared
 
 chmod +x $HOME/.boxware/boxware
 chmod +x $HOME/.boxware/boxwared
