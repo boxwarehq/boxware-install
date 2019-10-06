05625e9cfed79ea9951df1fe359a0a04e7fee0a3
Somesh Kar <someshkar04@gmail.com>
2019-10-06 16:10:23 +0530

    Init

---
+++ index.js
@@ -0,0 +1,24 @@
+const fastify = require('fastify')();
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+fastify.register(require('fastify-log'));
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/',
+});
+
+fastify.get('/', async (req, reply) => {
+  reply.type('text/plain').sendFile('install.sh');
+});
+
+fastify.get('*', async (req, reply) =>
+  reply.code(403).send(`You shouldn't be here`)
+);
+
+fastify.listen(port, err => {
+  if (err) throw fastify.error(err);
+  fastify.info(`Server listening on port ${port}`);
+});
