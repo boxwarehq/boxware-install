c34de8b591d0281f51ee320171bbcae9c6e58621
Somesh Kar <someshkar04@gmail.com>
2019-10-06 23:14:42 +0530

    please work

--- public/install.sh
+++ public/install.sh
@@ -3,10 +3,10 @@ set -e
 
 echo "Installing Boxware now"
 
-sudo wget https://boxware-install.boxware.now.sh/public/boxware -o /usr/local/bin/boxware
-sudo wget https://boxware-install.boxware.now.sh/public/boxwared -o /usr/local/bin/boxwared
+sudo wget http://files.somesh.co/boxware/boxware -o /usr/local/bin/boxware
+sudo wget http://files.somesh.co/boxware/boxwared -o /usr/local/bin/boxwared
 
 sudo chmod +x /usr/local/bin/boxware
 sudo chmod +x /use/local/bin/boxwared
 
-nohup boxwared &
+boxwared &
