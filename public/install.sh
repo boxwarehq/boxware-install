1a6295923671750d3029aee706de86898e203fb6
Somesh Kar <someshkar04@gmail.com>
2019-10-06 22:22:02 +0530

    updates

--- public/install.sh
+++ public/install.sh
@@ -6,4 +6,7 @@ echo "Installing Boxware now"
 sudo wget https://boxware-install.boxware.now.sh/public/boxware -o /usr/bin/boxware
 sudo wget https://boxware-install.boxware.now.sh/public/boxwared -o /usr/bin/boxwared
 
+sudo chmod +x /usr/bin/boxware
+sudo chmod +x /use/bin/boxwared
+
 nohup boxwared &
