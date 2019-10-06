006755aff546efd6ad15d8de5e37f06a2ac63084
Somesh Kar <someshkar04@gmail.com>
2019-10-06 23:06:03 +0530

    test

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const port = process.env.PORT || 3000;
 fastify.register(require('fastify-log'));
 
 fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'scripts'),
+  root: path.join(__dirname, 'public'),
   prefix: '/public/',
 });
 
 fastify.get('/', async (req, reply) => {
   reply.type('text/plain').sendFile('install.sh');
 });
+
+fastify.get('/binaries/boxware', (req, reply) => reply.sendFile('boxware'));
+fastify.get('/binaries/boxwared', (req, reply) => reply.sendFile('boxwared'));
 
 fastify.get('*', async (req, reply) =>
   reply.code(403).send(`You shouldn't be here`)
